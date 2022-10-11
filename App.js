f84aa64e9e95ea968d606ed39749d34dd4b1de8a
Sapna <sapna@deftsoft.com>
2022-10-11 15:25:50 +0530

    Email Validatin updated

--- App.js
+++ App.js
@@ -17,9 +17,11 @@ const App = () => {
     }
 
     if (!values.email) {
-      errors.email = "Email is required field";
-    } else if (values.email.length < 4) {
-      errors.email = "Email length must be greater than 5";
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
     }
 
     return errors;
